274931803fb0943d8f52008fc532f4d02fa6a48c
Joshua Thibeault <Tbow@ComputerofTime.local>
2018-06-16 09:30:37 -0400

    widget.js - change links from http to https in an attempt to fix gh-pages issue

--- widget.js
+++ widget.js
@@ -41,7 +41,7 @@ var opts_gauge = {
 if (window.jGauge === undefined) {
   var script_j = document.createElement('script');
   script_j.setAttribute("type","text/javascript");
-  script_j.setAttribute("src","http://bernii.github.io/gauge.js/dist/gauge.min.js");
+  script_j.setAttribute("src","https://bernii.github.io/gauge.js/dist/gauge.min.js");
   // Try to find the head, otherwise default to the documentElement
   (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_j);
 } else {
@@ -53,7 +53,7 @@ if (window.jGauge === undefined) {
 if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.4.2') {
   var script_tag = document.createElement('script');
   script_tag.setAttribute("type","text/javascript");
-  script_tag.setAttribute("src","http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+  script_tag.setAttribute("src","https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
     if (script_tag.readyState) {
       script_tag.onreadystatechange = function () { // For old versions of IE
         if (this.readyState == 'complete' || this.readyState == 'loaded') {
